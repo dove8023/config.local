fa6d09fb542444d4cb76f57420d050a2236cb58d
hexisen <616082046@qq.com>
2017-10-29 21:52:46 +0800

    fix bug, add test file.

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,23 @@
 let expect = require("chai").expect;
+let configData = require("./config/config.json");
+let configLocalData = require("./config/config.local.json");
 
-describe("first", function(){
+let config = require("../lib/index");
 
-    it("first go go.", (done)=>{
-        console.log("okokokok");
-        expect(1).to.be.equal(1);
+describe("config.local package test.", function(){
+
+    it("config.json value should be overwrited by config.local.json", (done)=>{
+        expect(config.name).to.be.equal(configLocalData.name);
         done();
-    })
+    });
+
+    it("config.json dosen't have attribute should be undefined.", (done)=>{
+        expect(config.sex).to.be.equal(undefined);
+        done();
+    });
+
+    it("the {{value}} should be transformed.", (done)=>{
+        expect(config.work.hisname).to.be.equal(config.name);
+        done();
+    });
 });
